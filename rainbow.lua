38038b31f1c85266cbac59259488b3ea249b813d
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2016-05-15 16:29:53 +1000

    Added a function

--- rainbow.lua
+++ rainbow.lua
@@ -11,3 +11,16 @@ local function ConsolePrintRainbow( text ) -- this prints rainbow text in the co
 	table.insert( tab, '\n' ) -- this adds a newline to the console, but it isn't really necessary, so you can remove it
 	MsgC( unpack( tab ) )
 end
+
+local function DrawRainbowOutlinedRect( x, y, w, h ) -- this draws a rainbow outline of a rectangle (using individual lines, ugh)
+	for i = x, x+w-1 do -- draw the x axis lines
+		surface.SetDrawColor( HSVToColor( i * multiplier % 360, 1, 1 ) )
+		surface.DrawLine( i, y, i+1, y )
+		surface.DrawLine( i, y+h, i+1, y+h )
+	end
+	for i = y, y+h-1 do -- draw the y axis lines
+		surface.SetDrawColor( HSVToColor( i * multiplier % 360, 1, 1 ) )
+		surface.DrawLine( x, i, x, i+1 )
+		surface.DrawLine( x+w, i, x+w, i+1 )
+	end
+end
