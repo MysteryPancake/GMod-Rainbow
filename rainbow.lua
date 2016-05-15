d513b7409074eb165f8f5831ff44cdafdc75c8ba
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2016-05-15 17:07:28 +1000

    Added another function

--- rainbow.lua
+++ rainbow.lua
@@ -1,18 +1,24 @@
-local multiplier = 20 -- this controls how much of the rainbow is shown with the following functions
+local multiplier = 20 -- This controls how much of the rainbow is shown with the following functions.
 
-local function ConsolePrintRainbow( text ) -- this prints rainbow text in the console
+-- Name: ConsolePrintRainbow
+-- Desc: This prints rainbow text in the console.
+
+local function ConsolePrintRainbow( text )
 	local tab = {}
 	for i = 1, #text do
-		local c = HSVToColor( i * multiplier % 360, 1, 1 )
-		table.insert( tab, c )
+		local col = HSVToColor( i * multiplier % 360, 1, 1 )
+		table.insert( tab, col )
 		local letter = string.sub( text, i, i )
 		table.insert( tab, letter )
 	end
-	table.insert( tab, '\n' ) -- this adds a newline to the console, but it isn't really necessary, so you can remove it
+	table.insert( tab, '\n' ) -- this adds a newline to the console, but this isn't essential
 	MsgC( unpack( tab ) )
 end
 
-local function DrawRainbowOutlinedRect( x, y, w, h ) -- this draws a rainbow outline of a rectangle (using individual lines, ugh)
+-- Name: DrawRainbowOutlinedRect
+-- Desc: This draws a rainbow outline of a rectangle (using individual lines).
+
+local function DrawRainbowOutlinedRect( x, y, w, h )
 	for i = x, x+w-1 do -- draw the x axis lines
 		surface.SetDrawColor( HSVToColor( i * multiplier % 360, 1, 1 ) )
 		surface.DrawLine( i, y, i+1, y )
@@ -22,5 +28,22 @@ local function DrawRainbowOutlinedRect( x, y, w, h ) -- this draws a rainbow out
 		surface.SetDrawColor( HSVToColor( i * multiplier % 360, 1, 1 ) )
 		surface.DrawLine( x, i, x, i+1 )
 		surface.DrawLine( x+w, i, x+w, i+1 )
+	end
+end
+
+-- Name: DrawRainbowRect
+-- Desc: This draws a filled rainbow rectangle using smaller rectangles. The bVertical (bool) argument defines whether this rainbow is vertical or not.
+
+local function DrawRainbowRect( bVertical, x, y, w, h )
+	if bVertical then
+		for i = y, y+h do
+			surface.SetDrawColor( HSVToColor( i * multiplier % 360, 1, 1 ) )
+			surface.DrawRect( x, i, w, 1 )
+		end
+	else
+		for i = x, x+w do
+			surface.SetDrawColor( HSVToColor( i * multiplier % 360, 1, 1 ) )
+			surface.DrawRect( i, y, 1, h )
+		end
 	end
 end
