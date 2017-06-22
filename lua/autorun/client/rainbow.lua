8c8205421d9dfd4fc94123be3188963068bc2828
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2017-06-22 18:19:57 +1000

    Mult -> Multiplier

--- lua/autorun/client/rainbow.lua
+++ lua/autorun/client/rainbow.lua
@@ -2,12 +2,12 @@
     ConsolePrintRainbow( number multiplier, string text )
     Prints rainbow text in the console.
 -----------------------------------------------------------]]
-local function ConsolePrintRainbow( mult, text )
+local function ConsolePrintRainbow( multiplier, text )
 	
 	local tab = {}
 	
 	for i = 1, #text do
-		local col = HSVToColor( i * mult % 360, 1, 1 )
+		local col = HSVToColor( i * multiplier % 360, 1, 1 )
 		table.insert( tab, col )
 		local letter = string.sub( text, i, i )
 		table.insert( tab, letter )
@@ -22,12 +22,12 @@ end
     ChatPrintRainbow( number multiplier, string text )
     Prints rainbow text in the chat.
 -----------------------------------------------------------]]
-local function ChatPrintRainbow( mult, text )
+local function ChatPrintRainbow( multiplier, text )
 	
 	local tab = {}
 	
 	for i = 1, #text do
-		local col = HSVToColor( i * mult % 360, 1, 1 )
+		local col = HSVToColor( i * multiplier % 360, 1, 1 )
 		table.insert( tab, col )
 		local letter = string.sub( text, i, i )
 		table.insert( tab, letter )
@@ -41,12 +41,12 @@ end
     DrawRainbowText( number multiplier, string text, string font, number x, number y )
     Draws rainbow text.
 -----------------------------------------------------------]]
-local function DrawRainbowText( mult, text, font, x, y )
+local function DrawRainbowText( multiplier, text, font, x, y )
 	
 	surface.SetFont( font )
 	
 	for i = 1, #text do
-		local col = HSVToColor( i * mult % 360, 1, 1 )
+		local col = HSVToColor( i * multiplier % 360, 1, 1 )
 		surface.SetTextColor( col )
 		local w, h = surface.GetTextSize( string.sub( text, 1, i - 1 ) )
 		surface.SetTextPos( x + w, y )
@@ -60,16 +60,16 @@ end
     DrawRainbowRectOutline( number multiplier, number x, number y, number width, number height )
     Draws a rainbow outline of a rectangle.
 -----------------------------------------------------------]]
-local function DrawRainbowRectOutline( mult, x, y, w, h )
+local function DrawRainbowRectOutline( multiplier, x, y, w, h )
 	
 	for i = x, x + w - 1 do
-		surface.SetDrawColor( HSVToColor( i * mult % 360, 1, 1 ) )
+		surface.SetDrawColor( HSVToColor( i * multiplier % 360, 1, 1 ) )
 		surface.DrawLine( i, y, i + 1, y )
 		surface.DrawLine( i, y + h - 1, i + 1, y + h )
 	end
 	
 	for i = y, y + h - 1 do
-		surface.SetDrawColor( HSVToColor( i * mult % 360, 1, 1 ) )
+		surface.SetDrawColor( HSVToColor( i * multiplier % 360, 1, 1 ) )
 		surface.DrawLine( x, i, x, i + 1 )
 		surface.DrawLine( x + w - 1, i, x + w, i + 1 )
 	end
@@ -80,15 +80,15 @@ end
     DrawRainbowRect( number multiplier, bool vertical, number x, number y, number width, number height )
     Draws a rainbow rectangle.
 -----------------------------------------------------------]]
-local function DrawRainbowRect( mult, vertical, x, y, w, h )
+local function DrawRainbowRect( multiplier, vertical, x, y, w, h )
 	if vertical then
 		for i = y, y + h - 1 do
-			surface.SetDrawColor( HSVToColor( i * mult % 360, 1, 1 ) )
+			surface.SetDrawColor( HSVToColor( i * multiplier % 360, 1, 1 ) )
 			surface.DrawRect( x, i, w, 1 )
 		end
 	else
 		for i = x, x + w - 1 do
-			surface.SetDrawColor( HSVToColor( i * mult % 360, 1, 1 ) )
+			surface.SetDrawColor( HSVToColor( i * multiplier % 360, 1, 1 ) )
 			surface.DrawRect( i, y, 1, h )
 		end
 	end
