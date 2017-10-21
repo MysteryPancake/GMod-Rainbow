606c8d0ac245d1690d96bf235a4622a389515926
MysteryPancake <MysteryPancake@users.noreply.github.com>
2017-10-21 14:49:31 +1100

    Multiplier -> Frequency
    Better

--- lua/autorun/client/rainbow.lua
+++ lua/autorun/client/rainbow.lua
@@ -1,13 +1,13 @@
 --[[---------------------------------------------------------
-    DrawRainbowText( number multiplier, string str, string font, number x, number y )
+    DrawRainbowText( number frequency, string str, string font, number x, number y )
     Draws rainbow text.
 -----------------------------------------------------------]]
-local function DrawRainbowText( multiplier, str, font, x, y )
+local function DrawRainbowText( frequency, str, font, x, y )
 	
 	surface.SetFont( font )
 	
 	for i = 1, #str do
-		surface.SetTextColor( HSVToColor( i * multiplier % 360, 1, 1 ) )
+		surface.SetTextColor( HSVToColor( i * frequency % 360, 1, 1 ) )
 		local w = surface.GetTextSize( string.sub( str, 1, i - 1 ) )
 		surface.SetTextPos( x + w, y )
 		surface.DrawText( string.sub( str, i, i ) )
@@ -16,15 +16,15 @@ local function DrawRainbowText( multiplier, str, font, x, y )
 end
 
 --[[---------------------------------------------------------
-    ChatPrintRainbow( number multiplier, string str )
+    ChatPrintRainbow( number frequency, string str )
     Prints rainbow text in the chat.
 -----------------------------------------------------------]]
-local function ChatPrintRainbow( multiplier, str )
+local function ChatPrintRainbow( frequency, str )
 	
 	local text = {}
 	
 	for i = 1, #str do
-		table.insert( text, HSVToColor( i * multiplier % 360, 1, 1 ) )
+		table.insert( text, HSVToColor( i * frequency % 360, 1, 1 ) )
 		table.insert( text, string.sub( str, i, i ) )
 	end
 
@@ -33,15 +33,15 @@ local function ChatPrintRainbow( multiplier, str )
 end
 
 --[[---------------------------------------------------------
-    ConsolePrintRainbow( number multiplier, string str )
+    ConsolePrintRainbow( number frequency, string str )
     Prints rainbow text in the console.
 -----------------------------------------------------------]]
-local function ConsolePrintRainbow( multiplier, str )
+local function ConsolePrintRainbow( frequency, str )
 	
 	local text = {}
 	
 	for i = 1, #str do
-		table.insert( text, HSVToColor( i * multiplier % 360, 1, 1 ) )
+		table.insert( text, HSVToColor( i * frequency % 360, 1, 1 ) )
 		table.insert( text, string.sub( str, i, i ) )
 	end
 	
@@ -52,20 +52,20 @@ local function ConsolePrintRainbow( multiplier, str )
 end
 
 --[[---------------------------------------------------------
-    DrawRainbowRect( number multiplier, bool vertical, number x, number y, number width, number height )
+    DrawRainbowRect( number frequency, bool vertical, number x, number y, number width, number height )
     Draws a rainbow rectangle.
 -----------------------------------------------------------]]
-local function DrawRainbowRect( multiplier, vertical, x, y, w, h )
+local function DrawRainbowRect( frequency, vertical, x, y, w, h )
 	
 	if vertical then
 		
 		for i = y, y + h - 1 do
-			surface.SetDrawColor( HSVToColor( i * multiplier % 360, 1, 1 ) )
+			surface.SetDrawColor( HSVToColor( i * frequency % 360, 1, 1 ) )
 			surface.DrawRect( x, i, w, 1 )
 		end
 	else
 		for i = x, x + w - 1 do
-			surface.SetDrawColor( HSVToColor( i * multiplier % 360, 1, 1 ) )
+			surface.SetDrawColor( HSVToColor( i * frequency % 360, 1, 1 ) )
 			surface.DrawRect( i, y, 1, h )
 		end
 		
@@ -87,19 +87,19 @@ local function DrawRainbowRect2( x, y, w, h )
 end
 
 --[[---------------------------------------------------------
-    DrawRainbowRectOutline( number multiplier, number x, number y, number width, number height )
+    DrawRainbowRectOutline( number frequency, number x, number y, number width, number height )
     Draws a rainbow outline of a rectangle.
 -----------------------------------------------------------]]
-local function DrawRainbowRectOutline( multiplier, x, y, w, h )
+local function DrawRainbowRectOutline( frequency, x, y, w, h )
 	
 	for i = x, x + w - 1 do
-		surface.SetDrawColor( HSVToColor( i * multiplier % 360, 1, 1 ) )
+		surface.SetDrawColor( HSVToColor( i * frequency % 360, 1, 1 ) )
 		surface.DrawLine( i, y, i + 1, y )
 		surface.DrawLine( i, y + h - 1, i + 1, y + h )
 	end
 	
 	for i = y, y + h - 1 do
-		surface.SetDrawColor( HSVToColor( i * multiplier % 360, 1, 1 ) )
+		surface.SetDrawColor( HSVToColor( i * frequency % 360, 1, 1 ) )
 		surface.DrawLine( x, i, x, i + 1 )
 		surface.DrawLine( x + w - 1, i, x + w, i + 1 )
 	end
